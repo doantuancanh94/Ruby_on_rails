eda75080378bbaae5334d063f933c81562473385
doantuancanh94 <doantuancanh94@gmail.com>
2018-11-29 16:11:14 +0700

    fix migrate

--- db/migrate/20181127024848_create_chapters.rb
+++ db/migrate/20181127024848_create_chapters.rb
@@ -3,12 +3,7 @@ class CreateChapters < ActiveRecord::Migration[5.2]
     create_table :chapters do |t|
       t.string :name
       t.text :content
-<<<<<<< HEAD
       t.references :stories, foreign_key: true
-=======
-      t.references :story, foreign_key: true
->>>>>>> ec48f90868da86b9543dde043403782053446103
-
       t.timestamps
     end
   end
